b83d1b28eb65dc49c6b4051232455a57947bf2f6
Kylee Tilley <kyleetilley@gmail.com>
2023-11-08 01:10:54 -0600

    Refactor docs

--- playground/src/components/example/example.tsx
+++ playground/src/components/example/example.tsx
@@ -1,32 +1,34 @@
+/* eslint-disable react/require-default-props */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
 import AceEditor from 'react-ace';
 
 interface EditorProps {
   children: string | string[];
+  height: string;
 }
 
 /**
  * Code editor component
  */
-const Example = ({ children }: EditorProps) => {
-  const lines = Array.isArray(children)
-    ? children.length
-    : (children ?? '').split('\n').length;
-
+const Example = ({ children, height }: EditorProps) => {
   const value = Array.isArray(children) ? children.join('\n') : children;
 
   return (
-    <AceEditor
-      className="h-100 font-monospace fs-6"
-      focus
-      mode="text"
-      name="editor"
-      readOnly
-      showPrintMargin={false}
-      maxLines={lines}
-      value={value}
-    />
+    <div style={{ height }}>
+      <AceEditor
+        className="h-100 font-monospace fs-6"
+        focus
+        mode="text"
+        name="editor"
+        readOnly
+        showPrintMargin={false}
+        highlightActiveLine={false}
+        cursorStart={0}
+        value={value}
+        width="100%"
+      />
+    </div>
   );
 };
 
--- playground/src/pages/docs.tsx
+++ playground/src/pages/docs.tsx
@@ -6,6 +6,26 @@ import { Link } from 'react-router-dom';
 import { Example } from '../components/example';
 import { Navbar } from '../components/navbar';
 
+interface DocCardProps {
+  title: string;
+  code: string | string[];
+  playgroundCode: string;
+  height: string;
+}
+
+const DocCard: React.FC<DocCardProps> = ({
+  title,
+  code,
+  playgroundCode,
+  height,
+}) => (
+  <div className="card p-2">
+    <h2 className="fs-3">{title}</h2>
+    <Example height={height}>{code}</Example>
+    <Link to={`/?code=${playgroundCode}`}>Playground</Link>
+  </div>
+);
+
 /**
  * Locks's documentation page component
  * @returns A page component
@@ -15,191 +35,142 @@ const Docs: React.FC = () => (
     <Navbar subBrandText="Docs" />
     <div className="m-4">
       <div className="vstack gap-4">
-        <div className="card p-2">
-          <h2 className="fs-3">Variables</h2>
+        <DocCard
+          title="Variables"
+          code={['var value = 123;', 'print value; // out: 123']}
+          playgroundCode="G4QwTgBKA2CuCmEC8ECMAmAzAbgFAAcwBLAOwBcoQ55sIB6OiAe1jIC40sg"
+          height="50px"
+        />
 
-          <div style={{ height: 50 }}>
-            <Example>
-              {['var value = 123;', 'print value; // out: 123']}
-            </Example>
-          </div>
+        <DocCard
+          title="Functions"
+          code={[
+            'fn sum (a, b) {',
+            '  return a + b;',
+            '}',
+            '',
+            'print sum(60, 40); // out: 100',
+          ]}
+          playgroundCode="GYOwBAzgrgtmAUBDANGARgSjAbwFBjACcBTAFykPETAGp0BuXAX11wAdCBLEUyWeAGwAGVABYhGemAD00sAHsopAFxgAjEKFA"
+          height="100px"
+        />
 
-          <Link to="/?code=G4QwTgBKA2CuCmEC8ECMAmAzAbgLACgAHMASwDsAXKEOebCAegYgHtYKAuNLIA">
-            Playground
-          </Link>
-        </div>
-        <div className="card p-2">
-          <h2 className="fs-3">Functions</h2>
+        <DocCard
+          title="String Concatenation"
+          code={[
+            'fn hello(name) {',
+            '  return "Hello " + name;',
+            '}',
+            '',
+            'print hello("World"); // out: Hello World',
+          ]}
+          playgroundCode="GYOwBAFgpgNjD2AKEBDAtlAlGA3gKDDACcoAXAVyPACIAJWBMasAajFQwG48BfPPAA5EAliFKQGSagHV4RGABNqmTmAD0asPHKkAXGHpx4YWfIVA"
+          height="100px"
+        />
 
-          <div style={{ height: 100 }}>
-            <Example>
-              {[
-                'fn sum (a, b) {',
-                '  return a + b;',
-                '}',
-                '',
-                'print sum(60, 40); // out: 100',
-              ]}
-            </Example>
-          </div>
+        <DocCard
+          title="Closures"
+          code={[
+            'fn greet(greeting) {',
+            '  fn person(name) {',
+            '    return greeting + " " + name;',
+            '  }',
+            '',
+            '  return person;',
+            '}',
+            '',
+            'print greet("Hello")("World"); // out: Hello World',
+          ]}
+          playgroundCode="GYOwBA5gTgpjAuAKad4EsQQJRgN4CgwxQwAHGKAZwHsREQBDAWxhwKKNngFcpwUEGCGADUYAEQTRYRiwDchMAF98irr3DkqtBSvykoGeJFgJE4gBIwANteris5gOrUo1gCYO5YAPQ+w1NzwAFxgVrbUYC5u7kA"
+          height="200px"
+        />
 
-          <Link to="/?code=GYOwBAzgrgtmAUBDANGARgSjAbwLACgwwAnAUwBcpjxEwBqdAbgIF8CCAHYgSxHMljwAbAAZUAFhEZGYAPSywAeyjkAXGACMIkUA">
-            Playground
-          </Link>
-        </div>
-        <div className="card p-2">
-          <h2 className="fs-3">String Concatenation</h2>
+        <DocCard
+          title="For Loops"
+          code={[
+            'for (var i = 0; i < 10; i = i + 1) {',
+            '  print i;',
+            '}',
+            '',
+            '// out: 0',
+            '// out: 1',
+            '// out: 2',
+            '// out: 3',
+            '// out: 4',
+            '// out: 5',
+            '// out: 6',
+            '// out: 7',
+            '// out: 8',
+            '// out: 9',
+          ]}
+          playgroundCode="GYewTgBAFAbghpAlhAvBADAbgsgPBARix1RIGpCBKCAbwCgIIAHMRAOwBcdM6BfOugHpBEEAFcOALgxCR4qYVmiJ0gExL50gMwaVEACy6FAViPSAbGYgB2KwA4rATiA"
+          height="300px"
+        />
 
-          <div style={{ height: 100 }}>
-            <Example>
-              {[
-                'fn hello(name) {',
-                '  return "Hello " + name;',
-                '}',
-                '',
-                'print hello("World"); // out: Hello World',
-              ]}
-            </Example>
-          </div>
+        <DocCard
+          title="If/Else"
+          code={[
+            'var isTrue = true;',
+            '',
+            'if (isTrue) {',
+            '  print "Was true!";',
+            '} else {',
+            '  print "Was false!";',
+            '}',
+            '',
+            '// out: Was true!',
+          ]}
+          playgroundCode="G4QwTgBAlgzgKmArgUwgXggFycg3AKHygDMIAKWBFASggG98IIAHMKAO0wgCIB1EGFhwBCbgQC+EZABsYqBk1YcufARGIhZyURMIB6PRAD2iTAC4I-QdhTCgA"
+          height="200px"
+        />
 
-          <Link to="/?code=GYOwBAFgpgNjD2AKEBDAtlAlGA3gWACgwwAnKAFwFcTwAiACVgTFrAGoxUMBuQgX0KEADiQCWIcpCZJaAdXgkYAE1qZuYAPQaw8SuQBcYRnHhh5ipUA">
-            Playground
-          </Link>
-        </div>
-        <div className="card p-2">
-          <h2 className="fs-3">Closures</h2>
+        <DocCard
+          title="Classes"
+          code={[
+            'class Greeter {',
+            '  init(greeting) {',
+            '    this.greeting = greeting;',
+            '  }',
+            '',
+            '  greet(name) {',
+            '    return this.greeting + " " + name;',
+            '  }',
+            '}',
+            '',
+            'var greeter = Greeter("Hello");',
+            '',
+            'print greeter.greet("World"); // out: Hello World',
+          ]}
+          playgroundCode="MYGwhgzhAEDiBOBTRAXR9oG8BQ1oEsA7fFACgHMlUjyBKLXPaFAC3wgDpLkUboBeaN2qFyAbkYBfbI2FlCYALaJ6OJtCQoArvELM2nOXwDU0AETnophcol5p07ADcwGOegFwqaeKTMAJRBAQAHszWglsAAd4IhQhb3Qubz8AdRD4EAATcLFoAHp86BCtFAAuaEDgkOh0zKygA"
+          height="300px"
+        />
 
-          <div style={{ height: 200 }}>
-            <Example>
-              {[
-                'fn greet(greeting) {',
-                '  fn person(name) {',
-                '    return greeting + " " + name;',
-                '  }',
-                '',
-                '  return person;',
-                '}',
-                '',
-                'print greet("Hello")("World"); // out: Hello World',
-              ]}
-            </Example>
-          </div>
-
-          <Link to="/?code=GYOwBA5gTgpjAuAKad4EsQQJRgN4FgAoMMUMABxigGcB7EREAQwFsYcDiSxZ4BXKOBQIMEMAGowAImkSwzNgG4iJAL4qwG3gPCUa9ZYXWEi5KBniRYCRFIASMADaPaUrLYDqtKI4AmbxTAAeiCwWj54AC4wB2daMC8fXyA">
-            Playground
-          </Link>
-        </div>
-        <div className="card p-2">
-          <h2 className="fs-3">For Loops</h2>
-
-          <div style={{ height: 300 }}>
-            <Example>
-              {[
-                'for (var i = 0; i < 10; i = i + 1) {',
-                '  print i;',
-                '}',
-                '',
-                '// out: 0',
-                '// out: 1',
-                '// out: 2',
-                '// out: 3',
-                '// out: 4',
-                '// out: 5',
-                '// out: 6',
-                '// out: 7',
-                '// out: 8',
-                '// out: 9',
-              ]}
-            </Example>
-          </div>
-
-          <Link to="/?code=GYewTgBAFAbghpAlhAvBADAbgsgPBARix1RIGpCBKCAbwFgAoCCABzEQDsAXHTRgX0ZA">
-            Playground
-          </Link>
-        </div>
-        <div className="card p-2">
-          <h2 className="fs-3">If/Else</h2>
-
-          <div style={{ height: 200 }}>
-            <Example>
-              {[
-                'var isTrue = true;',
-                '',
-                'if (isTrue) {',
-                '  print "Was true!";',
-                '} else {',
-                '  print "Was false!";',
-                '}',
-                '',
-                '// out: Was true!',
-              ]}
-            </Example>
-          </div>
-
-          <Link to="/?code=G4QwTgBAlgzgKmArgUwgXggFycg3AWACgioAzCAClgRQEoIBvIiCABzCgDtMIAiAdRAwsOAIS8ChAL4RkAGxiomhFuy48BQiKRALk4yVKJEA9CYgB7RJgBcEQcOwpRQAA">
-            Playground
-          </Link>
-        </div>
-        <div className="card p-2">
-          <h2 className="fs-3">Classes</h2>
-
-          <div style={{ height: 300 }}>
-            <Example>
-              {[
-                'class Greeter {',
-                '  init(greeting) {',
-                '    this.greeting = greeting;',
-                '  }',
-                '',
-                '  greet(name) {',
-                '    return this.greeting + " " + name;',
-                '  }',
-                '}',
-                '',
-                'var greeter = Greeter("Hello");',
-                '',
-                'print greeter.greet("World"); // out: Hello World',
-              ]}
-            </Example>
-          </div>
-
-          <Link to="/?code=MYGwhgzhAEDiBOBTRAXR9oG8CwAoa0AlgHaEoAUA5kqiZQJRZ4EEoAWhEAdNcindAC80XrWKUA3M2gBfPNNEViYALaJGOfCyQoArvGLR2nHjX7joAamgAiW1ejK1UrXNxu8ANzAZF6IXBm6OQ2ABKIICAA9jb0LngADvAkKCJB8KZ8IQDqUfAgACaxEtAA9KXQUbooAFzQ4ZFR0Ln5BXhAA">
-            Playground
-          </Link>
-        </div>
-        <div className="card p-2">
-          <h2 className="fs-3">Inheritance</h2>
-
-          <div style={{ height: 400 }}>
-            <Example>
-              {[
-                'class Greeter {',
-                '  init(greeting) {',
-                '    this.greeting = greeting;',
-                '  }',
-                '',
-                '  greet(name) {',
-                '    return this.greeting + " " + name;',
-                '  }',
-                '}',
-                '',
-                'class HelloGreeter < Greeter {',
-                '  init() {',
-                '    super.init("Hello");',
-                '  }',
-                '}',
-                '',
-                'var greeter = HelloGreeter();',
-                '',
-                'print greeter.greet("World"); // out: Hello World',
-              ]}
-            </Example>
-          </div>
-
-          <Link to="/?code=MYGwhgzhAEDiBOBTRAXR9oG8CwAoa0AlgHaEoAUA5kqiZQJRZ4EEoAWhEAdNcindAC80XrWKUA3M2gBfPNNEViYALaJGOfCyQoArvGLR2nHjX7joAamgAiW1ejK1UrXNxu8oSDAASiECAA9gh86NAAPHBmYZoEJGTkGtIEELoADuhc8RQ2fgGBNvQuBG4euABuYBiKYcJ5QSGo6IkueGnwJCgi0fCmfOQ2AOqB8CAAJoUS0AD009CBuigAXND1gdDDo2N4QA">
-            Playground
-          </Link>
-        </div>
+        <DocCard
+          title="Inheritance"
+          code={[
+            'class Greeter {',
+            '  init(greeting) {',
+            '    this.greeting = greeting;',
+            '  }',
+            '',
+            '  greet(name) {',
+            '    return this.greeting + " " + name;',
+            '  }',
+            '}',
+            '',
+            'class HelloGreeter < Greeter {',
+            '  init() {',
+            '    super.init("Hello");',
+            '  }',
+            '}',
+            '',
+            'var greeter = HelloGreeter();',
+            '',
+            'print greeter.greet("World"); // out: Hello World',
+          ]}
+          playgroundCode="MYGwhgzhAEDiBOBTRAXR9oG8BQ1oEsA7fFACgHMlUjyBKLXPaFAC3wgDpLkUboBeaN2qFyAbkYBfbI2FlCYALaJ6OJtCQoArvELM2nOXwDU0AETnophcol5p07KEgwAEohAgA9gh7poADxwVGgYagTEZKqMeBBaAA7oHEQkpGbunl5mtHbQDjIAbmAYcv6CGd6+qOikOTLx8EQoQiFJcmkA6l7wIAAm2WLQAPRD0F5aKABc0BVe0F09vUA"
+          height="400px"
+        />
       </div>
     </div>
   </>
