6677017be67d026251f7567607e81a4bde108b67
Kylee Tilley <kyleetilley@gmail.com>
2023-11-11 15:58:29 -0600

    Fill out tooling page

--- playground/src/pages/tooling.tsx
+++ playground/src/pages/tooling.tsx
@@ -12,7 +12,7 @@ const Tooling: React.FC = () => (
   <>
     <Navbar />
     <div className="m-4">
-      <div className="vstack gap-5">
+      <div className="vstack gap-4">
         <h2>
           <span
             className="me-2 bi bi-wrench text-black align-text-bottom"
@@ -21,7 +21,7 @@ const Tooling: React.FC = () => (
           />
           Language Tooling
         </h2>
-        <div>
+        <div className="shadow rounded p-3">
           <h3>
             <span
               className="me-2 bi bi-puzzle text-black align-text-bottom"
@@ -30,16 +30,48 @@ const Tooling: React.FC = () => (
             />
             VS Code Extension
           </h3>
-          See:{' '}
-          <a
-            href="https://github.com/kyleect/locks#vs-code-extension"
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://github.com/kyleect/locks#vs-code-extension
-          </a>
+          <p>
+            See:{' '}
+            <a
+              href="https://github.com/kyleect/locks#vs-code-extension"
+              target="_blank"
+              rel="noreferrer"
+            >
+              https://github.com/kyleect/locks#vs-code-extension
+            </a>
+          </p>
+
+          <h4>Download</h4>
+
+          <ol>
+            <li>
+              Download extension from latest passing{' '}
+              <a
+                href="https://github.com/kyleect/locks/actions/workflows/ci.yml"
+                target="_blank"
+                rel="noreferrer"
+              >
+                build
+              </a>
+              .
+            </li>
+          </ol>
+
+          <h4>Or Build & Install</h4>
+
+          <ol>
+            <li>Clone repo</li>
+            <li>
+              Run <code>just build-all</code>
+            </li>
+            <li>
+              Install extension from:{' '}
+              <code>vsc/out/locks-language-1.0.0.vsix</code>
+            </li>
+          </ol>
         </div>
-        <div>
+
+        <div className="shadow rounded p-3">
           <h3>
             <span
               className="me-2 bi bi-stack text-black align-text-bottom"
@@ -59,6 +91,17 @@ const Tooling: React.FC = () => (
               https://github.com/kyleect/locks#docker
             </a>
           </p>
+
+          <ol>
+            <li>Clone repo</li>
+            <li>
+              Run <code>docker build -t kyleect/locks:1.0.0 .</code>
+            </li>
+            <li>
+              Run{' '}
+              <code>docker run --rm -it kyleect/locks:1.0.0 locks repl</code>
+            </li>
+          </ol>
         </div>
       </div>
     </div>
